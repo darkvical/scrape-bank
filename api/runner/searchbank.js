4e6069ef2f5d5d4f20db882f4feac4d0e956a217
Victor Rodriguez <vical.rl@gmail.com>
2018-03-01 01:51:15 -0300

    Add dpendencias

--- api/runner/searchbank.js
+++ api/runner/searchbank.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const argv = require('minimist')(process.argv.slice(2));
+
 var firebase = require("firebase-admin");
 var serviceAccount = require("./accountbank-4f2d7-firebase-adminsdk.json");
 
@@ -9,20 +11,9 @@ var config = {
   };
 firebase.initializeApp(config);
 
+var dataPersist = { name: argv.user, age: 18 };
+
 var refExample = firebase.database().ref("example");
 var newNode = refExample.child("newnode");
-newNode.set({
-  name: 'Sasuke',
-  age: 18
-});
+newNode.set(dataPersist);
 
-/*var refCountries = firebase.database().ref("countries/");
-refCountries.orderByChild("name").once('value').then(function(countries) {
-    countries.forEach(function(country) {
-      console.log(country.val());
-    });
-    process.exit();
-});*/
-
-const argv = require('minimist')(process.argv.slice(2));
-console.log(argv.user);
