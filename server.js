b74c5a95e78f6d2a6ce87369a73d9a3286e814da
Victor Rodriguez <vical.rl@gmail.com>
2018-03-26 01:59:48 -0300

    CAmbios cors

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ var bodyParser = require('body-parser');
 var constants = require('./api/util/constants');
 var port = process.env.PORT || 8050;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 
